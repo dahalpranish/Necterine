677394029d3f8f94329d01cc23f432a2a4b5d3bd
Pranish Dahal <pranishdahal.dev@gmail.com>
2026-07-26 18:07:11 +0545

    complete log0/signup

--- Back_end/routers/login_register.py
+++ Back_end/routers/login_register.py
@@ -4,34 +4,34 @@ from sqlalchemy.orm import Session
 from Back_end.schemas import schemas
 from Back_end.models import userdata
 from Back_end.core import token,hashing
-from database import get_db
+from Back_end.database.database import get_db
 
 
 
-router = APIRouter(prefix="/user")
+router = APIRouter(prefix="/auth")
 
 
 
-@router.post("/resister",tags=[ "Register"],response_model=status.HTTP_200_OK)
+@router.post("/register",tags=[ "Register"],status_code=status.HTTP_200_OK)
 def resister(request:schemas.user_data,db: Session = Depends(get_db)):
-    existing_email = db.query(userdata.user_data).filter(userdata.user_data.email == request.email).first()
+    existing_email = db.query(userdata.user_data).filter(userdata.user_data.user_email == request.user_email).first()
     if existing_email:
         raise HTTPException(status_code=400, detail="Email already exists")
-    existing_username = db.query(userdata.user_data).filter(userdata.user_data.username == request.username).first()
+    existing_username = db.query(userdata.user_data).filter(userdata.user_data.user_name == request.user_name).first()
     if existing_username:
         raise HTTPException(status_code=400, detail="username already exists")
-    user = userdata.user_data(username = request.username, email = request.email, password = hashing.hash(request.password))
+    user = userdata.user_data(user_name = request.user_name, user_email = request.user_email, password = hashing.hash(request.password))
     
     db.add(user)
     db.commit()
     db.refresh(user)
-    return {"username": user.username, "email":user.email,"password":user.password}
+    return {"user_name": user.user_name, "user_email":user.user_email,"password":user.password}
 
-@router.post("/login", tags=["login_authentication"],response_model= status.HTTP_200_OK)
+@router.post("/login", tags=["login_authentication"],status_code= status.HTTP_200_OK)
 def login(request: schemas.user_login, db: Session = Depends(get_db)):
     user = db.query(userdata.user_data).filter(
-        (userdata.user_data.username == request.identifier) |
-        (userdata.user_data.email == request.identifier)
+        (userdata.user_data.user_name == request.identifier) |
+        (userdata.user_data.user_email == request.identifier)
     ).first()
 
     if not user:
@@ -40,5 +40,5 @@ def login(request: schemas.user_login, db: Session = Depends(get_db)):
     if not hashing.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
 
-    access_token = token.create_access_token(data={"sub": str(user.id)})
+    access_token = token.create_access_token(data={"sub": str(user.uid)})
     return {"access_token": access_token, "token_type": "bearer"}
---
+++ Back_end/schemas/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class user_data(BaseModel):
+    user_name : str
+    user_email : EmailStr
+    password : str =Field(max_length=72)
+
+class user_login(BaseModel):
+    identifier : str
+    password : str
